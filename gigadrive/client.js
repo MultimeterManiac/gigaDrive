f15853daa6e730515324e2da5f073035e66829d5
gerbil008 <163928546+gerbil008@users.noreply.github.com>
2024-12-09 19:15:18 +0100

    Create client.js

---
+++ gigadrive/client.js
@@ -0,0 +1,93 @@
+let socket = new WebSocket("wss://gigadrive.ddns.net:12356");
+let response = "";
+let wopened = false;
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function csend(msg){
+    socket.send(msg);
+}
+
+async function delete_file(filename){
+    if(wopened){
+        csend("d"+filename);
+        while(response == ""){
+            await sleep(10);
+        }
+        if(response == "d"){
+            return true;
+        }
+    }
+}
+
+async function list_files(){
+    if(wopened){
+        csend("l");
+    while(response == ""){
+        await sleep(10);
+    }
+    let rresponse = response;
+    response = "";
+    return rresponse;}
+}
+
+
+async function read_file(filename){
+    if(wopened){
+    csend("r" + filename+",");
+    while(response == ""){
+        await sleep(10);
+    }
+    let rresponse = response;
+    response = "";
+    return rresponse;}
+
+}
+
+async function write_file(filename, msg){
+    if(wopened){
+        csend("w"+filename+"%"+msg);
+        while(response == ""){
+            await sleep(10);
+        }
+        if(response == "d"){
+            return true;
+        }
+    }
+}
+
+async function make_file(filename){
+    if(wopened){
+        csend("m"+filename);
+        while(response == ""){
+            await sleep(10);
+        }
+        if(response == "d"){
+            return true;
+        }
+    }}
+
+
+socket.onopen = async function(e) {
+    wopened = true;
+};
+
+socket.onmessage = function(event) {
+    response = event.data;
+};
+
+socket.onclose = function(event) {
+    wopened = false;
+  if (event.wasClean) {
+    alert(`[error] please refresh page or contact developers`);
+  } else {
+    alert('[error] please refresh page or contact developers');
+  }
+};
+
+socket.onerror = function(error) {
+    wopened = false;
+  alert(`[error] please refresh page or contact developers`);
+};
