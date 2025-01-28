334acf42a8b1f6860f2b8371737401d5409d8f40
MulitmeterManiac <joasdo26@gmail.com>
2025-01-28 15:39:20 +0100

    tag1

--- gigadrive/website4.js
+++ gigadrive/website4.js
@@ -4,13 +4,13 @@ let username = "";
 let userr = "";
 
 const client = new Appwrite.Client();
-	client
-    .setEndpoint('https://cloud.appwrite.io/v1') // Replace with your Appwrite endpoint
-    .setProject('67571cd7002ca90a77ef'); // Replace with your Appwrite Project ID
+client
+	.setEndpoint('https://cloud.appwrite.io/v1') // Replace with your Appwrite endpoint
+	.setProject('67571cd7002ca90a77ef'); // Replace with your Appwrite Project ID
 
 const account = new Appwrite.Account(client);
 
-async function check(){
+async function check() {
 	updateUsedSpace();
 	try {
 		user = await account.get();
@@ -23,38 +23,47 @@ async function check(){
 	}
 }
 
-check();
+//check();
 
-async function logout(){
+async function logout() {
 	const res = await account.deleteSessions();
 	username = "";
 	user = "";
 	window.location.replace("https://gigadrive.ddns.net/login");
 }
 
-async function listFiles(){
+async function listFiles() {
 
-	let files = await list_files()
-	console.log(files);
-	files = JSON.parse(files);
-	//let files = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+	//let files = await list_files()
+	//console.log(files);
+	//files = JSON.parse(files);
+	let files =
+		[
+			{ "filename": "/user/file1.txt", "tag": "green" },
+			{ "filename": "/user/file2.txt", "tag": "blue" },
+			{ "filename": "/user/file3.txt", "tag": "red" },
+			{ "filename": "/user/file4.txt", "tag": "yellow" },
+			{ "filename": "/user/file5.txt", "tag": "white" },
+			{ "filename": "/user/file6.txt", "tag": "green" }
+		]
 	let par = document.getElementById("fileList");
-	
-	while (par.lastChild){
+
+	while (par.lastChild) {
 		par.removeChild(par.lastChild);
 	}
 
-	for (i in files){
+	for (i in files) {
 		//container
 		let parpar = document.createElement("div");
 		parpar.setAttribute("class", "fileLink");
+		parpar.classList.add(files[i].tag);
 		//filename
-			
+
 
 		let elficken = document.createElement("div");
 		elficken.setAttribute("class", "fileName");
 
-		let el2 = document.createTextNode(files[i].split("/")[files[i].split("/").length-1]);
+		let el2 = document.createTextNode(files[i].filename.split("/")[files[i].filename.split("/").length - 1]);
 		elficken.appendChild(el2);
 		//download button
 		let el3 = document.createElement("div");
@@ -77,7 +86,7 @@ async function listFiles(){
 		el10.appendChild(el11);
 
 
-		
+
 		parpar.appendChild(elficken);
 		//parpar.appendChild(el);
 		parpar.appendChild(el3);
@@ -87,7 +96,7 @@ async function listFiles(){
 	}
 }
 
-async function downloadFile(filename){
+async function downloadFile(filename) {
 	read_file(filename);
 }
 
@@ -97,24 +106,24 @@ async function deleteFile(filename) {
 }
 
 async function uploadFile() {
-	
+
 	write_file();
-	
+
 	listFiles();
 }
 
 let z;
-async function update(){
+async function update() {
 	updateUsedSpace();
 	let zz = await list_files();
 	console.log(zz);
-	if (z != zz){
+	if (z != zz) {
 		listFiles();
 		z = zz;
 	}
 }
 let pr = 0;
-async function updateUsedSpace(){
+async function updateUsedSpace() {
 	let l = document.getElementById("usedSpace");
 	let x = await get_full_space();
 	l.innerHTML = x;
@@ -136,21 +145,37 @@ setTimeout(listFiles, 1000);
 // for PWA: register serviceworker.js
 
 if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/service-worker.js")
-      .then(() => console.log("Service Worker Registered"))
-      .catch((error) => console.error("Service Worker Registration Failed:", error));
-  });
+	window.addEventListener("load", () => {
+		navigator.serviceWorker.register("/service-worker.js")
+			.then(() => console.log("Service Worker Registered"))
+			.catch((error) => console.error("Service Worker Registration Failed:", error));
+	});
 }
 
 // sigma. kp ob des funktioniert.
 function showUsername() {
 	document.getElementById('logoutLink').textContent = username;
 	document.getElementById('popupSettingsUsername').textContent = username;
- }
+}
 
- document.getElementById('addFile').addEventListener('change', function(e) {
+document.getElementById('addFile').addEventListener('change', function (e) {
 	document.getElementById("fffi").innerHTML = e.target.files.length + " files selected";
 	//console.log(e.target.files.length);
-  });
-  
+});
+
+function filter(tag) {
+
+
+	this.classlist.add("activeFilter");
+	let childs = document.querySelectorAll(".fileLink");
+	childs.forEach(function (child) {
+		if (child.classList.contains(tag)) {
+			child.classList.remove("hidden");
+			console.log("added");	
+		} else {
+			console.log("removed");
+			child.classList.add("hidden");
+		}
+	});
+}
+
